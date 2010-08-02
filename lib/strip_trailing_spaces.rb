efa3be2fc4677997127b10d2816fe786b1bcb7f8
Steven Hancock <stevenh512@gmail.com>
2010-08-02 12:27:34 -0700

    Oops, the plugin wasn't working. Fixed.

--- lib/strip_trailing_spaces.rb
+++ lib/strip_trailing_spaces.rb
@@ -2,29 +2,19 @@ module Redcar
   class StripTrailingSpaces
     def self.before_save(doc)
       if doc.mirror.is_a?(Redcar::Project::FileMirror)
-        read_cursor(doc)
-        doc.text = strip_spaces(doc)
-        set_cursor(doc)
+        # Read cursor position and adjust line offset
+        cursor_line = doc.cursor_line
+        line_offset = doc.cursor_line_offset
+        line = doc.get_line(cursor_line)
+        line_offset = line.rstrip.size if line_offset > line.rstrip.size
+
+        doc.text = doc.to_s.split("\n").each{|s| s.rstrip!}.join("\n")
+
+        # Adjust cursor offset and make visible
+        offset=doc.offset_at_line(cursor_line) + line_offset
+        doc.cursor_offset=offset
+        doc.ensure_visible(offset)
       end
-    end
-
-    private
-
-    def read_cursor(doc)
-      @cursor_line = doc.cursor_line
-      @line_offset = doc.cursor_line_offset
-      line = doc.get_line(@cursor_line)
-      @line_offset = line.rstrip.size if @line_offset > line.rstrip.size
-    end
-
-    def strip_spaces(doc)
-      doc.to_s.split("\n").each{|s| s.rstrip!}.join("\n")
-    end
-
-    def set_cursor(doc)
-      offset=doc.offset_at_line(@cursor_line) + @line_offset
-      doc.cursor_offset=offset
-      doc.ensure_visible(offset)
     end
   end
 end
